b6f23b6d863d9f0cf329433668982e23c6da126f
Nate Petersen <nate@ihatenate.net>
2012-10-09 19:18:30 -0500

    Initial Commit

---
+++ DropBlocks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DropBlocks
+//
+//  Created by Nate Petersen on 10/9/12.
+//  Copyright (c) 2012 Digital Rickshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DropBlocks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DropBlocks
+//
+//  Created by Nate Petersen on 10/9/12.
+//  Copyright (c) 2012 Digital Rickshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
